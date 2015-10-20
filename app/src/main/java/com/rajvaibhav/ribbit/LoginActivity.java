d6ea8d8a4a61cbfec95371f71c26562e1099f67b
Rajesh Mule <rajeshsmule@gmail.com>
2015-10-20 14:00:52 +0530

    intial commit

---
+++ app/src/main/java/com/rajvaibhav/ribbit/LoginActivity.java
@@ -0,0 +1,36 @@
+package com.rajvaibhav.ribbit;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.sax.StartElementListener;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    protected TextView mSignUpTextView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mSignUpTextView = (TextView) findViewById(R.id.signUpText);
+
+        mSignUpTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                 startActivity(intent);
+            }
+        });
+
+    }
+
+}
